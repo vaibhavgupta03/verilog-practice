e28a192ba81e1b39b5d32d2a8194f918e0858bbc
Vaibhav Gupta <vaibhavgupta6393@gmail.com>
2026-02-14 19:43:37 +0530

    Refactor PISO register module for improved readability and formatting

--- PISOreg/pisoreg.v
+++ PISOreg/pisoreg.v
@@ -9,8 +9,11 @@ module pisoreg(
     );
 
     wire [3:0] q_int;
+    wire [3:0] d_int;
+
     assign d_int[0] = (load_shift) ? 1'b0 : pi[0];
     
+
     assign d_int[1] = (load_shift) ? q_int[0] : pi[1];
     assign d_int[2] = (load_shift) ? q_int[1] : pi[2];
     assign d_int[3] = (load_shift) ? q_int[2] : pi[3];
@@ -20,5 +23,7 @@ module pisoreg(
     d_ff f2 (.clk(clk), .reset(reset), .D(d_int[2]), .Q(q_int[2]));
     d_ff f3 (.clk(clk), .reset(reset), .D(d_int[3]), .Q(q_int[3]));
 
+    // Final Output
     assign so = q_int[3];
+
 endmodule
